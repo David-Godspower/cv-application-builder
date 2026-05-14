8b6f86a07cfcabfeff843545906f85fcb53ab166
David Godspower Ajala <ajaladavid11@gmail.com>
2026-05-14 12:33:20 +0100

    add GeneralInfo component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import {useState} from 'react';
+import GeneralInfo from './components/GeneralInfo';
 
 function App(){
   return(
     <div>
       <h1>CV Application Builder</h1>
+      <GeneralInfo />
     </div>
   );
 }
