a15a322f471c5b9d94f1240c14a6e0ad19fd3619
David Godspower Ajala <ajaladavid11@gmail.com>
2026-05-14 15:12:30 +0100

    refactor: ensure CSS styles are imported in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {useState} from 'react';
 import GeneralInfo from './components/GeneralInfo';
 import Education from './components/Education';
 import Experience from './components/Experience';
+import './styles/index.css';
 
 function App() {
   return (
